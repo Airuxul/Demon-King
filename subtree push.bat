9cf63b53a053cd625e9c8351ea28a346da5ac8ce
Airuxul <804754746@qq.com>
2024-11-08 23:21:13 +0800

    Update subtree push.bat

--- subtree push.bat
+++ subtree push.bat
@@ -1,3 +1,2 @@
-git subtree push --prefix=CustomPackages/game-core https://github.com/Airuxul/game-core.git main
-git subtree push --prefix=CustomPackages/ui-proxy https://github.com/Airuxul/ui-proxy.git main
-git subtree push --prefix=CustomPackages/asset-proxy https://github.com/Airuxul/asset-proxy.git main
+git subtree push --prefix=CustomPackages/GameFramework https://github.com/Airuxul/GameFramework.git main
+pause
